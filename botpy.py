f1fb8b8716abf12545aff6c79c6fa833aebe7586
Halaiksse <63127846+Halaiksse@users.noreply.github.com>
2021-05-01 12:17:01 -0400

    Idk man
    shit added

--- botpy.py
+++ botpy.py
@@ -4,7 +4,8 @@ import discord
 from discord.ext import commands # This is just an extension to make commands a lot easier
 import requests
 import math
-import hypixel
+import random
+
 
 
 bot = commands.Bot(command_prefix=".", help_command=None) # You can set your prefix to be anything you desire!
@@ -18,8 +19,17 @@ async def on_ready(): # This function will be run by the discord library when th
 async def isOnline(ctx):
     await ctx.send("I am online !")
 
+@bot.command() 
+async def nword(ctx):
+    listWords = ["Nigeria", "Number", "Nun", "NCaliss que j'aime pas ma vie", "Niglet", "Philippe is no fun", "Nice", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "Near"]
+    await ctx.send(random.choice(listWords))
 
-
+@bot.command()
+async def MassPing(ctx, member: discord.Member=None):
+    if member is None:
+        member = ctx.message.author
+    for x in range(10):
+        await ctx.send( str(member.mention))
 
 
 @bot.command(aliases=['fuckinghelpme', 'help'])
@@ -31,35 +41,16 @@ async def Help(ctx):
     embed1.add_field(name="__.isOnline__", value="Sends a message if the bot is online.", inline=False)
     embed1.add_field(name="__.Help__", value="I think you can figure out this one by yourself.", inline=False)
     embed1.add_field(name="__.HowSus__", value="How sus am I ?", inline=False)
+    embed1.add_field(name="__.nword__", value="Generates a random N Word", inline=False)
+    embed1.add_field(name="__.MassPing__", value="Sends 10 ping to someone", inline=False)
     await ctx.send(embed=embed1)
 
-@bot.command()
+@bot.command(aliases=['howsus', 'Howsus'])
 async def HowSus(ctx):
     susrate = randrange(0, 101)
     author = ctx.message.author
 
-    await ctx.send("You are " + str(susrate) + " percent sus.")
-
-@bot.command()
-async def profile(ctx, name):
-    level = hypixel.get_level(name) # This is a reference the function we just created in hypixel.py
-    karma = hypixel.get_karma(name)
-    
-    if level is None: # Remember back in hypixel.py we returned 'None' if the API couldn't find a player
-        await ctx.send("Player not found! (Make sure to use their **Minecraft** username)") # This just sends a message to the channel the command was sent in from the bot
-    else: # We know that we found a player now because level is not None
-        embed2=discord.Embed(
-            title=f"Profile of {name}",
-            color=discord.Color.purple())
-        embed2.add_field(name="__Level__", value=f"{level}", inline=False)
-        embed2.add_field(name="__Karma__", value=f"{karma}", inline=False)
-        
-        await ctx.send(embed=embed2)
-        
-
-
-
-    
+    await ctx.send(  str(author.mention) + " is " + str(susrate) + " percent sus.")
 
 Fileapi = open("c:\\python\\API.txt", "r")
 APILIST = []
@@ -68,4 +59,4 @@ APILIST = Fileapi.readlines()
 
 
 # This just runs our bot
-bot.run(APILIST[1]) 
+bot.run(APILIST[0]) 
