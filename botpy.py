de0f20c764bf58c0afb0e16b6ef3b3e89df49ea5
Halaiksse <63127846+Halaiksse@users.noreply.github.com>
2021-10-20 14:23:00 -0400

    Update botpy.py

--- botpy.py
+++ botpy.py
@@ -19,11 +19,6 @@ async def on_ready(): # This function will be run by the discord library when th
 async def isOnline(ctx):
     await ctx.send("I am online !")
 
-@bot.command() 
-async def nword(ctx):
-    listWords = ["Nigeria", "Number", "Nun", "NCaliss que j'aime pas ma vie", "Niglet", "Philippe is no fun", "Nice", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "Near"]
-    await ctx.send(random.choice(listWords))
-
 @bot.command()
 async def MassPing(ctx, member: discord.Member=None):
     if member is None:
@@ -41,7 +36,6 @@ async def Help(ctx):
     embed1.add_field(name="__.isOnline__", value="Sends a message if the bot is online.", inline=False)
     embed1.add_field(name="__.Help__", value="I think you can figure out this one by yourself.", inline=False)
     embed1.add_field(name="__.HowSus__", value="How sus am I ?", inline=False)
-    embed1.add_field(name="__.nword__", value="Generates a random N Word", inline=False)
     embed1.add_field(name="__.MassPing__", value="Sends 10 ping to someone", inline=False)
     await ctx.send(embed=embed1)
 
@@ -51,12 +45,12 @@ async def HowSus(ctx):
     author = ctx.message.author
 
     await ctx.send(  str(author.mention) + " is " + str(susrate) + " percent sus.")
-
+# Local file containing the API key
 Fileapi = open("c:\\python\\API.txt", "r")
 APILIST = []
 APILIST = Fileapi.readlines()
 
 
 
-# This just runs our bot
+# This just runs our bot using a local file containing the API key.
 bot.run(APILIST[0]) 
